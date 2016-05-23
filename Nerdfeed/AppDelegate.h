ace1ea04aefe745e40f1d432016f287c05cc4075
Immanuel Kannan <immanueldk92@gmail.com>
2016-05-23 12:54:39 +0800

    Initial Commit

---
+++ Nerdfeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nerdfeed
+//
+//  Created by Immanuel Kannan on 23/05/2016.
+//  Copyright © 2016 Immanuel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
